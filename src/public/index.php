9876eb7bd2df6410ccf3c7d246e94900d525f883
Osa21ma <Osama.abdelfattah21@gmail.com>
2025-06-25 10:24:32 +0300

    first commit

---
+++ src/public/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+use os\Test\public\Request;
+use os\Test\public\App;
+require_once '../../vendor/autoload.php';
+require_once 'Session.php';
+$request = new Request;
+define('base_app', str_replace('\\','/',__DIR__).'/' );
+define('base_url','http://localhost:8080C43/lec/Restaurant_Project/src/public/index.php?');
+$app = new App($request);
+
