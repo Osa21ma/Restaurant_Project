22fbd7c91e6264700c3bc91c01bd4473d01785ad
Osa21ma <Osama.abdelfattah21@gmail.com>
2025-06-26 14:47:05 +0300

    sol error of orders

--- src/public/Session.php
+++ src/public/Session.php
@@ -8,7 +8,9 @@ class Session
     public function __construct()
     {
         
+        if(session_status() == PHP_SESSION_NONE){
             session_start(); 
+        } 
         
     }
     public function setSession($key, $value)
