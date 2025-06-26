9d33f84ca7afc317b393b80b64699debd6f40345
Osa21ma <Osama.abdelfattah21@gmail.com>
2025-06-26 12:56:08 +0300

    Display categories and menus ,link them together and display prices

---
+++ src/Controller/Sales.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace os\Test\Controller ;
+
+use os\Test\Model\Category;
+use os\Test\Model\Menu;
+use os\Test\Model\Order;
+use os\Test\public\Session;
+use os\Test\View;
+
+class Sales {
+    private $category;
+    private $menu;
+
+    private $order ;
+    private $session ;
+
+    public function __construct(){
+        $this->category= new Category();
+        $this->menu = new Menu();
+        // $this->order =new Order();
+        $this->session =new Session() ;
+
+    }
+    public function showSale(){
+        $allCategories = $this->category->getAllcategories();
+        $allMenues =$this->menu->getAllMenues();
+        View::Render('sales.php',['allcategories'=>$allCategories,'allMenues'=>$allMenues]);
+    }
+
+//     public function makeorder(){
+//         $orderData = filter_input_array(INPUT_POST);
+//       echo  $this->order->placeOrder($orderData);
+
+//     }
+
+//     public function showReceipt($id){
+//        $orderList = $this->order->getOrderList($id);
+//        $orderItem = $this->order->getOrderItem($id);
+//        $processdBy  =$this->session->getSession('username') ;
+//        View::Render('receipt.php', [
+//         'orderList' => $orderList,
+//         'orderItem' => $orderItem,
+//         'processdBy' => $processdBy
+//     ]);
+    
+//     }
+
+//     public function deleteOrder($id){
+//        echo  $this->order->rollback($id) ;
+//     }
+}
---
+++ src/Model/Category.php
@@ -0,0 +1,95 @@
+<?php 
+
+namespace os\Test\Model;
+
+use os\Test\public\Model;
+
+class Category extends Model{
+    protected $tableName ='category_list';
+      
+
+    public function getAllcategories(){
+        $query= " SELECT * FROM $this->tableName  WHERE status =1  AND  delete_flag =0  ORDER BY name ASC  " ;
+        $this-> query($query);
+        return $this->resultSet();
+    }
+
+    public function getcategories(){
+        $query= " SELECT * FROM $this->tableName   ORDER BY name ASC  " ;
+        $this-> query($query);
+        return $this->resultSet();
+    }
+
+    public function create($categoryData){
+        if (empty($categoryData['name']) || empty($categoryData['description']) || !isset($categoryData['status'])) {
+            return json_encode([
+                'status' => 'fail',
+                'msg' => 'All fields are required'
+            ]);
+        }
+        $query = "INSERT into $this->tableName (name , description , status )
+        values (:name , :description ,:status)" ;
+        $this->query($query);
+        $this->bind(':name' , $categoryData['name']);
+        $this->bind(':description' , $categoryData['description']);
+        $this->bind(':status' , $categoryData['status']);
+     
+        if($this->execute()){
+            return json_encode([
+                'status'=>'success',
+                'msg'=>'category has been created',
+            ]);
+
+        }else{
+            return json_encode([
+                'status'=>'fail',
+                'msg'=>'category has not been created',
+            ]);
+
+
+        }
+    }
+  
+
+    public function getCategoryDetail($id){
+        $query = "SELECT * from $this->tableName where id= $id" ;
+        $this->query($query);
+        return $this->single();
+    }
+
+    public function update($categoryData,$id){
+        if (empty($categoryData['name']) || empty($categoryData['description']) || !isset($categoryData['status'])) {
+            return json_encode([
+                'status' => 'fail',
+                'msg' => 'All fields are required'
+            ]);
+        }
+        $query = "UPDATE  $this->tableName SET name= :name , description= :description , status =:status WHERE id =$id" ;
+        $this->query($query);
+        $this->bind(':name' , $categoryData['name']);
+        $this->bind(':description' , $categoryData['description']);
+        $this->bind(':status' , $categoryData['status']);
+     
+        if($this->execute()){
+            return json_encode([
+                'status'=>'success',
+                'msg'=>'category has been UPDATE',
+            ]);
+
+        }else{
+            return json_encode([
+                'status'=>'fail',
+                'msg'=>'category has not been UPDATE',
+            ]);
+
+
+        }
+    }
+
+    public function delete($id){
+        $query = "DELETE FROM $this->tableName where id =$id";
+      $this->query($query);
+      return $this->execute();
+    }
+
+}
