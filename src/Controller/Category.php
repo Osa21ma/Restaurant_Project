27da163a19db835f7bf2a66ada3e800dc444e087
Osa21ma <Osama.abdelfattah21@gmail.com>
2025-06-27 16:47:16 +0300

    edit category

--- src/Controller/Category.php
+++ src/Controller/Category.php
@@ -32,11 +32,11 @@ class Category {
        $category= $this->category->getCategoryDetail($id);
         View::Render('viewCategory.php',['category'=>$category]);
     }
-    // public function editCategory($id){
+    public function editCategory($id){
 
-    //    $category= $this->category->getCategoryDetail($id);
-    //     View::Render('editCategory.php',['category'=>$category]);
-    // }
+       $category= $this->category->getCategoryDetail($id);
+        View::Render('editCategory.php',['category'=>$category]);
+    }
     // public function updateCategory($id){
     //     $categoryData =filter_input_array(INPUT_POST) ;
     //     echo $category= $this->category->update($categoryData,$id);
